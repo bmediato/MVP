0cdfc1e2a7e01a544544d37171c65c2ea5e435eb
Beatriz Mediato Aquino <scan.beatriz@voeazul.com.br>
2024-07-07 15:10:11 -0300

    feat: add database connection

---
+++ Infrastructure.Data/Usings.cs
@@ -0,0 +1,3 @@
+﻿global using Domain.Entities.v1.MongoDb.User;
+global using Domain.Interfaces.v1.Repositories.MongoDb.User;
+global using MongoDB.Driver;
---
+++ Infrastructure.Data/v1/MongoDb/Repositories/User/UserRepository.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Interfaces.v1.Configurations;
+
+namespace Infrastructure.Data.v1.MongoDb.Repositories.User;
+
+public class UserRepository : IUserMongoDbRepository 
+{
+    private readonly IMongoCollection<UserMongoDb> _user;
+
+    public UserRepository(IDatabaseConfig databaseConfig)
+    {
+        var client = new MongoClient(databaseConfig.ConnectionString);
+        var database = client.GetDatabase(databaseConfig.DatabaseName);
+
+        _user = database.GetCollection<UserMongoDb>("users");
+    }
+
+    public async Task UpsertAsync(UserMongoDb user)
+    {
+        await _user.InsertOneAsync(user);
+    }
+}
